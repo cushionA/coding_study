fec7a24a521530ff4ec248c7558a260332c105b4
cushionA <tatuki.m1105@gmail.com>
2026-07-05 22:38:23 +0900

    learn(ml-intro): unit01/ex01 合格 (attempts=1, tier=0)

--- courses/ml-intro/unit01-numpy-basics/ex01_arrays.py
+++ courses/ml-intro/unit01-numpy-basics/ex01_arrays.py
@@ -9,17 +9,17 @@ import numpy as np
 # (C#: Enumerable.Range(1, n).ToArray() に相当)
 def make_range(n):
     # TODO: 範囲から配列を作る(ループ不要。詰まったらヒント参照)
-    raise NotImplementedError
+    return np.arange(1,n+1)
 
 
 # 配列の全要素を2倍して返す
 # (C#: arr.Select(x => x * 2).ToArray() に相当するが、ループもLINQも不要)
 def double_all(arr):
     # TODO: ループを書かずに全要素を2倍する
-    raise NotImplementedError
+    return arr * 2
 
 
 # 配列の合計・平均・最大値をタプル (sum, mean, max) で返す
 def summarize(arr):
     # TODO: 統計量ごとに専用の関数/メソッドがある。ループで自作しない
-    raise NotImplementedError
+    return (arr.sum(), arr.mean(), arr.max())
